fc08113c7b97714b17187471cd31cdd00620208a
Kristoffer Larsson <Kristoffer.larsson01@gmail.com>
2023-05-02 19:18:01 +0200

    Cleaned up grund_traning method and menu

--- app/grund_traning.py
+++ app/grund_traning.py
@@ -5,8 +5,6 @@ import winsound
 class grund_traning(tk.Button):  # Grundtraining class
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
-        self.config(width=50, height=2, padx=10, pady=10, bg="#3264a8", font=("Arial", 10))
-        self["text"] = "Grundträning"
         self["command"] = self.grund_traning_command
 
     def grund_traning_command(self):
--- app/menu.py
+++ app/menu.py
@@ -12,29 +12,23 @@ class menu():
     def menu(self):
         root = tk.Tk()
         root.title("Grundträning - ett redskap mot stress")
-
         label_frame = tk.Frame(root)
         label = tk.Label(root, text="Main menu", width= 22, font=("Arial", 24), bg="#3264a8")
         label.pack(side="top")
-
         button_frame = tk.Frame(root)
         button_frame.pack()
 
         # Create custom buttons for each button in the list
         button_list = []
         for i, value in enumerate(["Grundträning", "Fokusering", "Historik", "Kalender", "Exit"]):
-            if value == "Grundträning":
-                button = grund_traning(button_frame)  # GRUNDTRÄNINGSKLASSS
-            else:
-                button = tk.Button(button_frame, text=value, width= 50, height=2, padx=10, pady=10, bg="#3264a8", font=("Arial", 10))
+            button = tk.Button(button_frame, text=value, width= 50, height=2, padx=10, pady=10, bg="#3264a8", font=("Arial", 10))
             button.grid(row=i)
             button_list.append(button)
-
         # add event listener to kalender button
+        button_list[0].config(command=grund_traning(button_frame).grund_traning_command)
         button_list[3].config(command=self.btn_calender_pressed)
         # Allows quit button to exit
         button_list[-1].config(command=root.destroy)
-
         root.mainloop()
 
     def btn_calender_pressed(self):
