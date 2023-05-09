012c143c98d349f33f666a5a9edb1f03f3518253
Kristoffer Larsson <Kristoffer.larsson01@gmail.com>
2023-05-09 10:36:56 +0200

    Fixed test classes

--- test/test_fokusering.py
+++ test/test_fokusering.py
@@ -6,21 +6,20 @@ import tkinter as tk
 import winsound
 # Add the parent directory to the sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from grund_traning import grund_traning
+from fokusering import fokusering
 
 
-class TestGrundTraining(unittest.TestCase):
+class Test_fokusering(unittest.TestCase):
     @patch("winsound.PlaySound")
-    def test_grund_traning_command(self, mock_PlaySound):
+    def test_fokusering_command(self, mock_PlaySound):
         # Create the necessary test objects
         parent = tk.Tk()
-        button = grund_traning(parent)
+        button = fokusering(parent)
         # Simulate button click
-        button.grund_traning_command()
+        button.fokusering_command()
         # Verify that PlaySound was called with the correct arguments
         expected_calls = [
-            call("zero_to_ten_and_back.wav", winsound.SND_FILENAME),
-            call("Count.wav", winsound.SND_FILENAME)
+            call("Fokusering.wav", winsound.SND_FILENAME),
         ]
         mock_PlaySound.assert_has_calls(expected_calls)
 
