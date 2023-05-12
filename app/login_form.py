31ec43daa96ef95e9106c5276759dfbc56fa7131
Tobias <tobias.vinblad0013@stud.hkr.se>
2023-05-13 01:19:53 +0200

    updated pswd to be stored hashed, also made application backward compatible with old stored pswd

--- app/login_form.py
+++ app/login_form.py
@@ -106,7 +106,6 @@ class login_form:
             uppercase=1,  # need min. 1 uppercase letters
             numbers=1,  # need min. 1 digits
             special=1,  # need min. 1 special characters
-            nonletters=1,  # need min.1 non-letter characters (digits, specials, anything)
         )
         result = policy.test(pswd)
 
