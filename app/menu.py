18b124366bdca164569b33010adecd0d1d09fab0
Kristoffer Larsson <Kristoffer.larsson01@gmail.com>
2023-05-17 13:08:23 +0200

    Removed historik, changed button_list so it match

--- app/menu.py
+++ app/menu.py
@@ -22,7 +22,7 @@ class menu:
 
         # Create custom buttons for each button in the list
         button_list = []
-        for i, value in enumerate(["Grundträning", "Fokusering", "Historik", "Kalender", "Ange stressnivå", "Exit"]):
+        for i, value in enumerate(["Grundträning", "Fokusering", "Kalender", "Ange stressnivå", "Exit"]):
             button = tk.Button(button_frame, text=value, width= 50, height=2, padx=10, pady=10, bg="#3264a8", font=("Arial", 10))
             button.grid(row=i)
             button_list.append(button)
@@ -32,9 +32,9 @@ class menu:
         # Button for "Fokusering"
         button_list[1].config(command=fokusering(button_frame).fokusering_command)
         # add event listener to stressnivå button
-        button_list[3].config(command=self.btn_calender_pressed)
+        button_list[2].config(command=self.btn_calender_pressed)
         # add event listener to kalender button
-        button_list[4].config(command=self.btn_stress_pressed)
+        button_list[3].config(command=self.btn_stress_pressed)
 
         # Allows quit button to exit
         button_list[-1].config(command=root.destroy)
